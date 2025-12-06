e42162420ce8e3a490f68eb87e72f4b5d6779d52
Camille Gohier <camille.gohier@outlook.fr>
2025-12-07 00:43:24 +0100

    Juste gestion des messages sans inscription ni connexion

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const http = require("http");
 const WebSocket = require("ws");
 const { createClient } = require("@supabase/supabase-js");
 
@@ -11,70 +12,20 @@ const server = new WebSocket.Server({ port: PORT });
 
 console.log("Serveur WebSocket lancé sur le port ", PORT);
 
-async function inscription(username, password) {
-    await bdd.from("users").insert({ username, password });
-}
-
-async function connexion(username, password) {
-    return await bdd.from("users").select("*").eq("username", username).eq("password", password).single();
-}
-
-
 server.on("connection", ws => {
-    ws.user = null;
 
-    ws.on("message", async dataRaw => {
+    ws.on("message", dataRaw => {
         const msg = dataRaw.toString();
-        const args = msg.split(" ");
-        const request = args[0].toUpperCase();
+        
+        const [username, text] = msg.split("|");
 
-        if (request == "INSCRIPTION") {
-            const username = args[1];
-            const password = args[2];
-
-            if (!username || !password) {
-                return ws.send("Pseudo ou Mot de passe manquant");
-            }
-
-            const error = await inscription(username, password);
-
-            if (error) {
-                ws.send("Erreur inscription");
-            }
-            else {
-                ws.send("Inscription OK");
-            }
-            return;
-        }
-
-        if (request == "CONNEXION") {
-            const username = args[1];
-            const password = args[2];
-
-            if (!username || !password) {
-                return ws.send("Pseudo ou Mot de passe manquant");
-            }
-
-            const { data, error } = await connexion(username, password);
-
-            if (error || !data) {
-                ws.send("Erreur connexion");
-            }
-            else {
-                ws.user = username;
-                ws.send("Connexion OK")
-            }
-            return;
-        }
-
-        if (!ws.user) {
-            ws.send("Utilisateur pas connecté");
-            return;
+        if (!username || !text) {
+            return ws.send("Format invalide");
         }
 
         server.clients.forEach(client => {
             if (client.readyState == WebSocket.OPEN) {
-                client.send(ws.user + " : " + msg);
+                client.send(username + " : " + text);
             }
         })
     })
