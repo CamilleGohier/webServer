228a4e77efcba46f7afd495ad499aa67aca7ef9f
Camille Gohier <camille.gohier@outlook.fr>
2025-12-07 19:38:40 +0100

    Gestion de plusieurs salons indépendants (normalement)

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require("http");
+// const http = require("http");
 const WebSocket = require("ws");
 const { createClient } = require("@supabase/supabase-js");
 
@@ -10,23 +10,34 @@ const bdd = createClient(SUPABASE_URL, SUPABASE_KEY);
 const PORT = process.env.PORT || 10000;
 const server = new WebSocket.Server({ port: PORT });
 
+const rooms = new Map();
+
 console.log("Serveur WebSocket lancé sur le port ", PORT);
 
-server.on("connection", ws => {
+server.on("connection", (ws, request) => {
+    const args = new URLSearchParams(request.url.split('?')[1]);
+    const roomId = args.get("room");
+
+    if (!rooms.has(roomId)) {
+        rooms.set(roomId, new Set());
+    }
+    rooms.get(roomId).add(ws);
 
     ws.on("message", dataRaw => {
-        const msg = dataRaw.toString();
-        
-        const [username, text] = msg.split("|");
+        const msg = JSON.parse(dataRaw);
 
-        if (!username || !text) {
-            return ws.send("Format invalide");
+        if (!msg.username || !msg.text) {
+            return ws.send("Pseudo ou message manquant");
         }
 
-        server.clients.forEach(client => {
+        rooms.get(roomId).forEach(client => {
             if (client.readyState == WebSocket.OPEN) {
-                client.send(username + " : " + text);
+                client.send(msg.username + " : " + msg.text);
             }
         })
+    })
+
+    ws.on('close', () => {
+        rooms.get(roomId).delete(ws);
     })
 });
